214bf2fba75f7f8f5bdd8fa0d50bb11e2c3a019f
Md Sohail <mdsohail855141@gmail.com>
2026-07-29 18:30:23 +0530

    second commit

--- src/day_1/Positve_negative_Zero.java
+++ src/day_1/Positve_negative_Zero.java
@@ -1,2 +1,7 @@
-package day_1;public class Positve_negative_Zero {
+package day_1;
+
+public class Positve_negative_Zero {
+    public static void main(String[] args) {
+        System.out.println("hello wold");
+    }
 }
