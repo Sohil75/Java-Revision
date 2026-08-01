c73f0aad40db81997ac8ab0b052d4e2fbb88daec
Md Sohail <mdsohail855141@gmail.com>
2026-08-01 14:59:16 +0530

    day 2 building mini project

---
+++ src/BankingProject/BankingProject.java
@@ -0,0 +1,50 @@
+package BankingProject;
+
+import java.util.Scanner;
+
+public class BankingProject {
+
+    static int pin =4125;
+    static int accountNumber = 4545468;
+    int balance =0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Online Indian Banking.... ");
+        System.out.println("Enter you Account Number: ");
+        int acNum= sc.nextInt();
+        System.out.println("Enter pin: ");
+        int password = sc.nextInt();
+
+        if(acNum == accountNumber && password== pin ){
+            System.out.println("Select what brings you here today: ");
+            System.out.println(1 + " Deposit");
+            System.out.println(2 + " Withdraw");
+            System.out.println(3 + " CheckBalance");
+            System.out.println(0 + " Exit");
+            int option = sc.nextInt();
+            switch (option){
+                case 1: {
+                    System.out.println("deposit");
+                    break;
+                }
+                case 2: {
+                    System.out.println("withdraw");
+                    break;
+                }
+                case 3:{
+                    System.out.println("checkbalance");
+                    break;
+                }
+                default:{
+                    System.out.println("Exit");
+                }
+
+            }
+
+        }
+        else {
+            System.out.println("Check Your Credentials.. ");
+        }
+
+    }
+}
